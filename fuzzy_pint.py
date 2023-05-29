d2fc00ce43dd667d75f2128dcef0526e282b1d35
Eric Moyer <eric@lemoncrab.com>
2023-05-29 07:55:23 -0700

    wip:multiply

--- fuzzy_pint.py
+++ fuzzy_pint.py
@@ -43,16 +43,15 @@ def main():
     print(f'{indent}{v1*i1=}')
 
 
-
 class FuzzyPint:
     def __init__(self, magnitude: float, units: str = None, err_p: float = 0.0, err_n: float = 0.0):
-        assert err_p > 0
+        assert err_p >= 0
         assert err_n <= 0
         units = UREG(units) if isinstance(units, str) else units
         self._quantity = magnitude * units
         self._err_p = err_p
         self._err_n = err_n
-    
+
     def __add__(self, b):
         quantity = self._quantity + b._quantity
         err_p = self._err_p + b._err_p
@@ -61,15 +60,45 @@ class FuzzyPint:
 
     def __mul__(self, b):
         quantity = self._quantity * b._quantity
-        err_p = self._err_p + b._err_p
-        err_n = self._err_n + b._err_n
+        err_p, err_n = FuzzyPint._get_error(b, _multiply)
+        # err_p = self._err_p + b._err_p
+        # err_n = self._err_n + b._err_n
         return FuzzyPint(quantity.m, quantity.units, err_p, err_n)
+    
+    @classmethod
+    def _get_error(a: 'FuzzyPint', b: 'FuzzyPint', function):
+        a_m = a._quantity.m
+        b_m = b._quantity.m
+        nominal_m = function(a_m, b_m)
+
+        errors = []
+        for a_m_speculative, b_m_speculative in (
+            (a_m + a._err_p, b_m + b._err_p),
+            (a_m + a._err_n, b_m + b._err_p),
+            (a_m + a._err_p, b_m + b._err_n),
+            (a_m + a._err_n, b_m + b._err_n)
+        ):
+            result = function(a_m_speculative, b_m_speculative)
+            error = result - nominal_m
+            errors.append(error)
+        err_p = max(errors)
+        err_n = min(errors)
+        assert err_p >= 0
+        assert err_n <= 0
+
+        return err_p, err_n
 
     def __repr__(self):
-        return f'<FuzzyPint({self._quantity.m}, {self._quantity.units}, {self._err_p}, {self._err_n})>'
-    
+        return (
+            f'<FuzzyPint({self._quantity.m}, {self._quantity.units}, {self._err_p}, {self._err_n})>'
+        )
+
     def __str__(self):
         return f'{self._quantity} [+{self._err_p}, {self._err_n}]'
+
+
+def _multiply(a, b):
+    return a * b
 
 
 if __name__ == "__main__":
